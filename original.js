977962331f947876f841077d9876416792cc4e8a
gzhou <891855179@qq.com>
2024-12-06 19:59:44 +0800

    merge-pdf.cdtools.click

--- original.js
+++ original.js
@@ -24,7 +24,8 @@ const links = [
         title: 'PDF工具',
         icon: '📄',
         items: [
-            { text: 'PDF转扫描件', url: 'https://scan.cdtools.click', description: '能够让 PDF 看起来就像是扫描件一样的纯前端网站' }
+            { text: 'PDF转扫描件', url: 'https://scan.cdtools.click', description: '能够让 PDF 看起来就像是扫描件一样的纯前端网站' },
+            { text: 'PDF合并工具', url: 'https://merge-pdf.cdtools.click', description: '在线PDF合并工具 - 快速、免费、安全的文档处理解决方案' }
         ]
     },
     {
@@ -52,6 +53,7 @@ const links = [
             { text: '学生证生成器', url: 'https://studentid.cdtools.click', description: '用户可以自定义学生证信息，并通过交互式界面实时预览效果。' },
             { text: '国家2000坐标系转换', url: 'https://cgcs2000.cdtools.click', description: 'WGS-84 至 CGCS2000 坐标转换器' },
             { text: '静谧空间', url: 'https://peace.cdtools.click', description: '静谧空间(peace.cdtools.click)为您提供优质的白噪声、双耳节拍、自然环境声和冥想音频。包括白噪声、双耳节拍、蓝噪声、棕噪声、瀑布声等多种助眠音效，帮助您放松身心，改善睡眠质量。' },
+            { text: '火车座位靠窗查询工具', url: 'https://seat.cdtools.click', description: '火车座位靠窗查询工具，帮助您查询火车座位是否靠窗。' },
         ]
     },
     {
